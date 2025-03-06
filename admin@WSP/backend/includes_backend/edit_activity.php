2b11295b503105b43539907c75474d4b7a2d7a4f
chinnarat07 <bu.chinnarat@gmail.com>
2025-03-06 11:07:58 +0700

    12

--- admin@WSP/backend/includes_backend/edit_activity.php
+++ admin@WSP/backend/includes_backend/edit_activity.php
@@ -91,7 +91,7 @@ if (isset($_GET['p_id'])) {
                                     </div>-->
             <!--x-->
             <div class="form-group col-lg-12">
-                <label for="activity_image" class="d-block ms-3 fw-bold ms-3">Activityc Image</label>
+                <label for="activity_image" class="d-block ms-3 fw-bold ms-3">Activity Image</label>
                 <div>
                     <label  for="activity_image" class="upload-icon">
                         <span style="margin-left: 8px ;">เลือกไฟล์รูปภาพ</span> <i class="fa fa-file-image-o" aria-hidden="true" style="font-size: 2.3rem;"></i>
